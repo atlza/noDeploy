5c36ac8d91bb7557993b843b34f655da50d101cf
Guillaume <gleroy.zla@gmail.com>
2019-11-28 14:37:12 +0100

    cleaner initial commit

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Get the aliases and functions
+if [ -f ~/.bashrc ]; then
+        . ~/.bashrc
+fi
+
+# Replace "username" by your username
+ME="deploy"
+
+#environnement argument
+environnement=$1
+
+echo "*******************************************************"
+echo "Launching deploy as ${ME} in $environnement "
+echo "*******************************************************"
+
+if [ -z $environnement ]; then
+    echo "Environnement is missing"
+    exit -1
+elif [ $environnement != 'prod' ] && [ $environnement != 'recette' ]; then
+    echo "Environnement is wrong, should be prod or recette"
+    exit -1
+fi
+
+#sudo deploy
+sudo -i -u $ME bash -c "./realDeploy.sh $environnement "
